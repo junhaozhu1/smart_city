5f9a7d4998d5a3c6dc39a105ab91bcecdb2c8d1b
junhaozhu1 <130132433+junhaozhu1@users.noreply.github.com>
2024-04-05 15:54:55 +1100

    new city map

--- createMap.py
+++ createMap.py
@@ -1,32 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def drawMap(csv_path = 'map_data.csv'):
+def drawMap(csv_path = 'city_map.csv'):
     # 从 CSV 文件中读取地图数据
     df = pd.read_csv(csv_path, index_col=False)
 
     # 提取医院、学校、街道和地铁线路的数据
     hospitals = df[df['Type'] == 'Hospital']
     schools = df[df['Type'] == 'School']
+    residential_buildings = df[df['Type'] == 'Residential_building']
+    malls = df[df['Type'] == 'Mall']
+    clinics = df[df['Type'] == 'Clinic']
+    parks = df[df['Type'] == 'Park']
+    industrial_areas = df[df['Type'] == 'Industrial_area']
+    police_stations = df[df['Type'] == 'Police_station']
+    fire_stations = df[df['Type'] == 'Fire_station']
     streets = df[df['Type'] == 'Street']
     subways = df[df['Type'] == 'Subway']
+
+    building_list = [hospitals, schools, residential_buildings, malls, clinics, parks, industrial_areas, police_stations, fire_stations]
 
     def write_label(start_x, start_y, end_x, end_y, row):
         if start_x == end_x and start_y != end_y:
             if row['Type'] == 'Street':
-                ax.text(start_x, ((start_y + end_y) / 2)+0.5, row['Name'], rotation=90, fontsize=10)
+                ax.text(start_x, ((start_y + end_y) / 2)+0.5, row['Name'], rotation=90, fontsize=7)
             else:
-                ax.text(start_x, (start_y + end_y) / 2, row['Name'], rotation=90, fontsize=10)
+                ax.text(start_x, (start_y + end_y) / 2, row['Name'], rotation=90, fontsize=7)
         elif start_y == end_y and start_x != start_y:
             if row['Type'] == 'Street':
-                ax.text((start_x + end_x) / 2, start_y, row['Name'], fontsize=10)
+                ax.text((start_x + end_x) / 2, start_y, row['Name'], fontsize=7)
             else:
-                ax.text((start_x + end_x) / 2, start_y+0.5, row['Name'], fontsize=10)
+                ax.text((start_x + end_x) / 2, start_y+0.5, row['Name'], fontsize=7)
         elif start_x != end_x and start_y != end_y:
             if row['Type'] == 'Street':
-                ax.text((start_x + end_x) / 2, ((start_y + end_y) / 2)+1, 'Street', fontsize=10)
+                ax.text((start_x + end_x) / 2, ((start_y + end_y) / 2)+1, 'Street', fontsize=7)
             else:
-                ax.text((start_x + end_x) / 2, (start_y + end_y) / 2, row['Name'], fontsize=10)
+                ax.text((start_x + end_x) / 2, (start_y + end_y) / 2, row['Name'], fontsize=7)
 
     # 绘制地图
     fig, ax = plt.subplots(figsize=(10, 8))
@@ -46,17 +55,24 @@ def drawMap(csv_path = 'map_data.csv'):
         write_label(start_x, start_y, end_x, end_y, row)
 
     # draw buildings
-    for i, row in hospitals.iterrows():
-        if row['Type'] == 'Hospital':
-            ax.scatter(row['Start_X'], row['Start_Y'], color='red', marker='H', s=200, label='Hospital')
-            ax.text(row['Start_X'], row['Start_Y'], row['Name'], fontsize=12)
+        
+    for building in building_list:
+        for i, row in building.iterrows():
+            ax.scatter(row['Start_X'], row['Start_Y'], color=row['color'], marker='o', s=50, label=row['Type'])
+            ax.text(row['Start_X'], row['Start_Y'], row['Name'], fontsize=7)
+    
 
-    for i, row in schools.iterrows():
-        ax.scatter(row['Start_X'], row['Start_Y'], color='blue', marker='o', s=200, label='School')
-        ax.text(row['Start_X'], row['Start_Y'], row['Name'], fontsize=12)
+    # for i, row in hospitals.iterrows():
+    #     # if row['Type'] == 'Hospital':
+    #     ax.scatter(row['Start_X'], row['Start_Y'], color='red', marker='H', s=200, label='Hospital')
+    #     ax.text(row['Start_X'], row['Start_Y'], row['Name'], fontsize=12)
+
+    # for i, row in schools.iterrows():
+    #     ax.scatter(row['Start_X'], row['Start_Y'], color='blue', marker='o', s=200, label='School')
+    #     ax.text(row['Start_X'], row['Start_Y'], row['Name'], fontsize=12)
 
     # 设置标题和坐标轴标签
-    ax.set_title('Simulated Map with Hospitals, Schools, Streets and Subways')
+    ax.set_title('City map')
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
 
